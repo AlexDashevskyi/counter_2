a5ef48422044cc3bf07c53353d213eacf4438347
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-07-29 14:17:47 -0700

    Updates

---
+++ precache-manifest.b867eeb041ffee20d184dad20eaa6d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff99ee4b9b409de36eba2cfc238835f1",
+    "url": "/counter_2/index.html"
+  },
+  {
+    "revision": "df712eda64ab1d76d16c",
+    "url": "/counter_2/static/css/main.fc2f16e7.chunk.css"
+  },
+  {
+    "revision": "1b388b5d75a3465721b8",
+    "url": "/counter_2/static/js/2.e704e5cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter_2/static/js/2.e704e5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df712eda64ab1d76d16c",
+    "url": "/counter_2/static/js/main.96fa54bc.chunk.js"
+  },
+  {
+    "revision": "e0a1f2acc7d5d4e30d70",
+    "url": "/counter_2/static/js/runtime-main.145bcc34.js"
+  }
+]);
